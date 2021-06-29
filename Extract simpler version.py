2490701aeb41c2350baaa6be53d202c503d64c91
maikeal <32373310+maikeal@users.noreply.github.com>
2021-06-28 22:51:44 -0400

    Update Extract simpler version.py

--- Extract simpler version.py
+++ Extract simpler version.py
@@ -6,10 +6,10 @@ import sqlite3
 from datetime import datetime, timedelta
 
 # Set the Twitter API authentication
-api = twitter.Api(consumer_key='eTwR2lbd6dMxCq988wtWnBvGC',
-                  consumer_secret='IEsaSATulZcZyBE6QWxzK97dBU12wB7ORbVBk7kqUrYZNTfOxM',
-                  access_token_key='1404645710058987522-2tHLCZnMazsLg86yBKL3s5ZUrHX6hB',
-                  access_token_secret='B1YbGzAFbRrAOhLS1867uJIVza30v4GLifQIOb7gA04Cm')
+api = twitter.Api(consumer_key='bkjbjkbdbbkdjjbebfebbdejkbdjebdjkbek',
+                  consumer_secret='jbdjebjdebdfbeivfvevfevfvvfevjkvfevifvevfiejk',
+                  access_token_key='jwdgwoidgwodudevdkejvbifkedvwldlwblkbkbkldb',
+                  access_token_secret='jbdjebdbebdbefbdebfevfvebfeubfke,bfelkbflebeblje')
 
 # set a handle
 handle = 'aoc'
